baddadda7acc4f0dd89cabd40451ab84e3551407
abiyyuafief <153440450+abiyyuafief@users.noreply.github.com>
2023-12-11 20:48:59 +0700

    Add files via upload

---
+++ CreateTriangle.java
@@ -0,0 +1,19 @@
+package part1;
+
+public class CreateTriangle {
+    public static void main (String [] args){
+        //Looping bersarang
+
+        for(int i =0; i<10; i++){
+            for(int j=0; j<10; j++){
+                System.out.print("* ");
+                if (i==j){
+                    break;
+                } else if ((i+j)==9){
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
